ac2021d0cdd864cb5206ee1438d218a42f6bd215
MhmoudGit <mahmoud.a.a.y18@gmail.com>
2023-01-18 00:28:33 +0200

    dups

--- ContainsDublicates.js
+++ ContainsDublicates.js
@@ -1,5 +1,16 @@
-const ContainsDublicates = (nums) => {
-
+const ContainsDuplicates = (nums) => {
+    // sets doesnt contain duplicates
+    const noDups = new Set(nums)
+    let goal = []
+    noDups.forEach((one) => goal.push(one))
+    console.log(nums);
+    console.log(goal);
+    // hence the length of goal and nums should differ if nums[] has duplicates its length will be greater than goal[] which doesnt has any dubs
+	if (nums.length === goal.length) {
+        return false
+    } else {
+        return true
+    }
 }
 
-console.log(ContainsDublicates([1,2,3,1])); 
+console.log(ContainsDuplicates([1,1,1,3,3,4,3,2,4,2]))
