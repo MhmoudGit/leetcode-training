e6ffed878e21207a63c4a7552c9232cfbd020b91
MhmoudGit <mahmoud.a.a.y18@gmail.com>
2023-01-20 19:41:51 +0200

    trial

---
+++ MergeSortedArray.js
@@ -0,0 +1,9 @@
+const merge = (nums1, m, nums2, n) => {
+    nums1.length = m
+    nums2.length = n
+    let sorted = nums1.concat(nums2)
+    sorted.sort()
+    return sorted
+};
+
+console.log(merge([0],0 , [1], 1));
